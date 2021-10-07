736c1f4907720a0439372d0fbe7937f39aa3004a
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-07 20:29:43 +0330

    Importing relevant packages

--- all/pl_net.py
+++ all/pl_net.py
@@ -2,3 +2,7 @@
     Convolutional Neural Network with Parallel Layers
     Author: Milad Sadeghi DM - EverLookNeverSee@GitHub.com
 """
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import (Conv2D, Dense, BatchNormalization, Concatenate,
+                                     Flatten, MaxPooling2D, AveragePooling2D, Input)
