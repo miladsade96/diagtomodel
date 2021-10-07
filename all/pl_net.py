f707bc4c95e962037d676a5c2ce5a6ca684bf23d
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-07 20:55:50 +0330

    Defining the second parallel layer

--- all/pl_net.py
+++ all/pl_net.py
@@ -15,3 +15,8 @@ input_ = Input(shape=(224, 224, 3))
 in_1 = Conv2D(filters=16, kernel_size=(3, 3), activation="relu", padding="same")(input_)
 conv_1 = BatchNormalization()(in_1)
 conv_1 = AveragePooling2D(pool_size=(2, 2), strides=(3, 3))(conv_1)
+
+# Defining the second parallel layer
+in_2 = Conv2D(filters=16, kernel_size=(5, 5), activation="relu", padding="same")(input_)
+conv_2 = BatchNormalization()(in_2)
+conv_2 = AveragePooling2D(pool_size=(2, 2), strides=(3, 3))(conv_2)
