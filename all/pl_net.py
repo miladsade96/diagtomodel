7af124391935c9b1b10f5e080616d381087f97d0
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-07 20:31:20 +0330

    Defining input layer

--- all/pl_net.py
+++ all/pl_net.py
@@ -6,3 +6,7 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import (Conv2D, Dense, BatchNormalization, Concatenate,
                                      Flatten, MaxPooling2D, AveragePooling2D, Input)
+
+
+# Defining input layer
+input_ = Input(shape=(224, 224, 3))
