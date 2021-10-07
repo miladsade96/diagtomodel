7b25df0284232266c07f7f24aab1393e5204b656
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-07 21:03:38 +0330

    Defining the fourth parallel layer

--- all/pl_net.py
+++ all/pl_net.py
@@ -25,3 +25,8 @@ conv_2 = AveragePooling2D(pool_size=(2, 2), strides=(3, 3))(conv_2)
 in_3 = Conv2D(filters=16, kernel_size=(5, 5), activation="relu", padding="same")(input_)
 conv_3 = BatchNormalization()(in_3)
 conv_3 = MaxPooling2D(pool_size=(2, 2), strides=(3, 3))(conv_3)
+
+# Defining the fourth parallel layer
+in_4 = Conv2D(filters=16, kernel_size=(9, 9), activation="relu", padding="same")(input_)
+conv_4 = BatchNormalization()(in_4)
+conv_4 = MaxPooling2D(pool_size=(2, 2), strides=(3, 3))(conv_4)
