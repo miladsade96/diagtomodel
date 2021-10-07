a900a3615c52ca07adb1b38bc51125637ecef4d4
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-07 20:47:10 +0330

    Defining the first parallel layer

--- all/pl_net.py
+++ all/pl_net.py
@@ -10,3 +10,8 @@ from tensorflow.keras.layers import (Conv2D, Dense, BatchNormalization, Concaten
 
 # Defining input layer
 input_ = Input(shape=(224, 224, 3))
+
+# Defining the first parallel layer
+in_1 = Conv2D(filters=16, kernel_size=(3, 3), activation="relu", padding="same")(input_)
+conv_1 = BatchNormalization()(in_1)
+conv_1 = AveragePooling2D(pool_size=(2, 2), strides=(3, 3))(conv_1)
