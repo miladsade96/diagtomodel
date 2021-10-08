12354ac51c7b9c9e7d3675244bce7caa2387638e
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-08 09:46:24 +0330

    Importing relevant packages

--- all/mla_net.py
+++ all/mla_net.py
@@ -2,3 +2,8 @@
     Convolutional Neural Network with Multiple Layer Additions
     Author: Milad Sadeghi Dm - EverLookNeverSee@GitHub.com
 """
+
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.activations import relu, softmax
+from tensorflow.keras.layers import Conv2D, Dense, Input, GlobalAvgPool2D, Flatten, MaxPooling2D, Add
