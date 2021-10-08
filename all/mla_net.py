452baac2c81290687797985e965425021b610b95
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-08 09:48:39 +0330

    Defining convolutional layers up to second addition

--- all/mla_net.py
+++ all/mla_net.py
@@ -17,3 +17,10 @@ conv_1 = Conv2D(filters=16, kernel_size=(3, 3), strides=(2, 2), activation=relu,
 conv_2 = Conv2D(filters=16, kernel_size=(1, 1), strides=(4, 4), activation=relu, padding="same")(input_layer)
 conv_3 = Conv2D(filters=16, kernel_size=(3, 3), strides=(2, 2), activation=relu, padding="same")(conv_1)
 first_add = Add()([conv_2, conv_3])
+
+first_pool = MaxPooling2D(pool_size=(3, 3), strides=2)(first_add)
+# Defining convolutional layers up to second addition
+conv_4 = Conv2D(filters=32, kernel_size=(3, 3), strides=(1, 1), activation=relu, padding="same")(first_pool)
+conv_5 = Conv2D(filters=32, kernel_size=(1, 1), strides=(1, 1), activation=relu, padding="same")(first_pool)
+conv_6 = Conv2D(filters=32, kernel_size=(3, 3), strides=(1, 1), activation=relu, padding="same")(conv_4)
+second_add = Add()([conv_5, conv_6])
