99d86fe549a48102e0663a5b7fc5fda78af63176
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-08 09:47:00 +0330

    Defining the input layer

--- all/mla_net.py
+++ all/mla_net.py
@@ -7,3 +7,7 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.activations import relu, softmax
 from tensorflow.keras.layers import Conv2D, Dense, Input, GlobalAvgPool2D, Flatten, MaxPooling2D, Add
+
+
+# Defining the input layer
+input_layer = Input(shape=(720, 720, 3))
