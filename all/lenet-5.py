66735ef1bc4179b42025c0bcd22dda466588c471
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-11 17:46:01 +0330

    Implementing rest of the model

--- all/lenet-5.py
+++ all/lenet-5.py
@@ -11,3 +11,18 @@ from tensorflow.keras.layers import Input, Conv2D, AveragePooling2D, Dense, Flat
 
 # Defining the input layer
 input_layer = Input(shape=(32, 32, 1))
+
+conv_1 = Conv2D(filters=6, kernel_size=(5, 5), activation=tanh)(input_layer)
+first_pool = AveragePooling2D(pool_size=(2, 2))(conv_1)
+conv_2 = Conv2D(filters=16, kernel_size=(5, 5), activation=tanh)(first_pool)
+second_pool = AveragePooling2D(pool_size=(2, 2))(conv_2)
+flat = Flatten()(second_pool)
+d_1 = Dense(units=120, activation=tanh)(flat)
+d_2 = Dense(units=84, activation=tanh)(d_1)
+output_layer = Dense(units=10, activation=softmax)(d_2)
+
+lenet_5 = Model(inputs=[input_layer], outputs=[output_layer], name="LeNet-5")
+
+
+if __name__ == '__main__':
+    lenet_5.summary()
