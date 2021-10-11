b61b096d1500e6655ffbb03542cf261d79441221
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-11 17:25:15 +0330

    Adding initial docstring

---
+++ all/lenet-5.py
@@ -0,0 +1,5 @@
+"""
+    Implementation of LeNet-5 Model Architecture
+    Paper: https://yann.lecun.com/exdb/publis/pdf/lecun-98.pdf
+    Author: Milad Sadeghi DM - EverLookNeverSee@gitHub.com
+"""
