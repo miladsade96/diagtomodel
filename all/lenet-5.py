d1b37fdb766083bba69382b269f8f295ec390407
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-11 17:27:40 +0330

    Importing relevant packages

--- all/lenet-5.py
+++ all/lenet-5.py
@@ -3,3 +3,7 @@
     Paper: https://yann.lecun.com/exdb/publis/pdf/lecun-98.pdf
     Author: Milad Sadeghi DM - EverLookNeverSee@gitHub.com
 """
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.activations import tanh, softmax
+from tensorflow.keras.layers import Input, Conv2D, AveragePooling2D, Dense, Flatten
