28d5e98524146b3aedaf1339cfcc411681664ad9
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-12 20:55:13 +0330

    Adding initial docstring

---
+++ all/alexnet.py
@@ -0,0 +1,5 @@
+"""
+    Implementation of AlexNet Model Architecture
+    Paper: https://papers.nips.cc/paper/2012/file/c399862d3b9d6b76c8436e924a68c45b-Paper.pdf
+    Author: Milad Sadeghi DM - EverLookNeverSee@GitHub.com
+"""
