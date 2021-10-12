8f8843ba16fe6d036d8775e1a172baf313d2b2a2
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-12 21:12:48 +0330

    Importing relevant packages

--- all/alexnet.py
+++ all/alexnet.py
@@ -3,3 +3,8 @@
     Paper: https://papers.nips.cc/paper/2012/file/c399862d3b9d6b76c8436e924a68c45b-Paper.pdf
     Author: Milad Sadeghi DM - EverLookNeverSee@GitHub.com
 """
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.activations import relu, softmax
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Input
