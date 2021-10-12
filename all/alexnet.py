4f72b0223ce80bd269f4fb865ce46111a660f1cb
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-12 21:46:34 +0330

    Implementing the model layers

--- all/alexnet.py
+++ all/alexnet.py
@@ -8,3 +8,18 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.activations import relu, softmax
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Input
+
+model = Sequential(name="AlexNet")
+model.add(Input(shape=(224, 224, 3)))
+model.add(Conv2D(filters=96, kernel_size=(11, 11), activation=relu))
+model.add(MaxPooling2D(pool_size=(3, 3)))
+model.add(Conv2D(filters=256, kernel_size=(5, 5), activation=relu))
+model.add(MaxPooling2D(pool_size=(3, 3)))
+model.add(Conv2D(filters=384, kernel_size=(3, 3), activation=relu))
+model.add(Conv2D(filters=384, kernel_size=(3, 3), activation=relu))
+model.add(Conv2D(filters=256, kernel_size=(3, 3), activation=relu))
+model.add(MaxPooling2D(pool_size=(3, 3)))
+model.add(Flatten())
+model.add(Dense(units=4096, activation=relu))
+model.add(Dense(units=4096, activation=relu))
+model.add(Dense(units=1000, activation=softmax))
