0b24ef1df19fec366216263afff0364ea7c40e69
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-13 21:26:07 +0330

    Defining first conv block

--- all/vgg-16.py
+++ all/vgg-16.py
@@ -12,3 +12,8 @@ from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, MaxPooling2D
 
 # Defining the input layer
 input_layer = Input(shape=(224, 224, 3))
+
+# Defining first Conv block
+conv_1 = Conv2D(filters=64, kernel_size=(3, 3), activation=relu, padding="same")(input_layer)
+conv_2 = Conv2D(filters=64, kernel_size=(3, 3), activation=relu, padding="same")(conv_1)
+pooling_1 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(conv_2)
