6211805d613ea105473e8945bcc17acf16be3c71
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-13 21:21:11 +0330

    Importing relevant packages

--- all/vgg-16.py
+++ all/vgg-16.py
@@ -3,3 +3,8 @@
     Paper: https://arxiv.org/pdf/1409.1556
     Author: Milad Sadeghi DM - EverLookNeverSee@GitHub.com
 """
+
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.activations import relu, softmax
+from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, MaxPooling2D
