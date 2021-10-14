5a99508c9544add7ad9d66f7f4561450a232eff2
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-14 20:28:34 +0330

    Fixing paddings

--- all/vgg-16.py
+++ all/vgg-16.py
@@ -16,24 +16,24 @@ input_layer = Input(shape=(224, 224, 3))
 # Defining first conv block
 conv_1 = Conv2D(filters=64, kernel_size=(3, 3), activation=relu, padding="same")(input_layer)
 conv_2 = Conv2D(filters=64, kernel_size=(3, 3), activation=relu, padding="same")(conv_1)
-pooling_1 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(conv_2)
+pooling_1 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding="same")(conv_2)
 
 # Defining second conv block
 conv_3 = Conv2D(filters=128, kernel_size=(3, 3), activation=relu, padding="same")(pooling_1)
 conv_4 = Conv2D(filters=128, kernel_size=(3, 3), activation=relu, padding="same")(conv_3)
-pooling_2 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(conv_4)
+pooling_2 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding="same")(conv_4)
 
 # Defining third conv block
 conv_5 = Conv2D(filters=256, kernel_size=(3, 3), activation=relu, padding="same")(pooling_2)
 conv_6 = Conv2D(filters=256, kernel_size=(3, 3), activation=relu, padding="same")(conv_5)
 conv_7 = Conv2D(filters=256, kernel_size=(3, 3), activation=relu, padding="same")(conv_6)
-pooling_3 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(conv_7)
+pooling_3 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding="same")(conv_7)
 
 # Defining fourth cnv block
 conv_8 = Conv2D(filters=512, kernel_size=(3, 3), activation=relu, padding="same")(pooling_3)
 conv_9 = Conv2D(filters=512, kernel_size=(3, 3), activation=relu, padding="same")(conv_8)
 conv_10 = Conv2D(filters=512, kernel_size=(3, 3), activation=relu, padding="same")(conv_9)
-pooling_4 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(conv_10)
+pooling_4 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding="same")(conv_10)
 
 # Defining fifth conv block
 conv_11 = Conv2D(filters=512, kernel_size=(3, 3), activation=relu, padding="same")(pooling_4)
