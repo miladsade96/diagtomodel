d237d1cd7e958895202c338c536991e1d663516e
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-14 19:41:43 +0330

    Defining third conv block

--- all/vgg-16.py
+++ all/vgg-16.py
@@ -13,7 +13,7 @@ from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, MaxPooling2D
 # Defining the input layer
 input_layer = Input(shape=(224, 224, 3))
 
-# Defining first Conv block
+# Defining first conv block
 conv_1 = Conv2D(filters=64, kernel_size=(3, 3), activation=relu, padding="same")(input_layer)
 conv_2 = Conv2D(filters=64, kernel_size=(3, 3), activation=relu, padding="same")(conv_1)
 pooling_1 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(conv_2)
@@ -22,3 +22,9 @@ pooling_1 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(conv_2)
 conv_3 = Conv2D(filters=128, kernel_size=(3, 3), activation=relu, padding="same")(pooling_1)
 conv_4 = Conv2D(filters=128, kernel_size=(3, 3), activation=relu, padding="same")(conv_3)
 pooling_2 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(conv_4)
+
+# Defining third conv block
+conv_5 = Conv2D(filters=256, kernel_size=(3, 3), activation=relu, padding="same")(pooling_2)
+conv_6 = Conv2D(filters=256, kernel_size=(3, 3), activation=relu, padding="same")(conv_5)
+conv_7 = Conv2D(filters=256, kernel_size=(3, 3), activation=relu, padding="same")(conv_6)
+pooling_3 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(conv_7)
