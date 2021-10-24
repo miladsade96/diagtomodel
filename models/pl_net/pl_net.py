249cff16729cf3940ca75d969124eebd70fcd366
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-10-24 12:08:38 +0330

    Adding padding flag to pooling layers

--- models/pl_net/pl_net.py
+++ models/pl_net/pl_net.py
@@ -14,22 +14,22 @@ input_ = Input(shape=(224, 224, 3))
 # Defining the first parallel layer
 in_1 = Conv2D(filters=16, kernel_size=(3, 3), activation="relu", padding="same")(input_)
 conv_1 = BatchNormalization()(in_1)
-conv_1 = AveragePooling2D(pool_size=(2, 2), strides=(3, 3))(conv_1)
+conv_1 = AveragePooling2D(pool_size=(2, 2), strides=(3, 3), padding="same")(conv_1)
 
 # Defining the second parallel layer
 in_2 = Conv2D(filters=16, kernel_size=(5, 5), activation="relu", padding="same")(input_)
 conv_2 = BatchNormalization()(in_2)
-conv_2 = AveragePooling2D(pool_size=(2, 2), strides=(3, 3))(conv_2)
+conv_2 = AveragePooling2D(pool_size=(2, 2), strides=(3, 3), padding="same")(conv_2)
 
 # Defining the third parallel layer
 in_3 = Conv2D(filters=16, kernel_size=(5, 5), activation="relu", padding="same")(input_)
 conv_3 = BatchNormalization()(in_3)
-conv_3 = MaxPooling2D(pool_size=(2, 2), strides=(3, 3))(conv_3)
+conv_3 = MaxPooling2D(pool_size=(2, 2), strides=(3, 3), padding="same")(conv_3)
 
 # Defining the fourth parallel layer
 in_4 = Conv2D(filters=16, kernel_size=(9, 9), activation="relu", padding="same")(input_)
 conv_4 = BatchNormalization()(in_4)
-conv_4 = MaxPooling2D(pool_size=(2, 2), strides=(3, 3))(conv_4)
+conv_4 = MaxPooling2D(pool_size=(2, 2), strides=(3, 3), padding="same")(conv_4)
 
 # Concatenating the layers
 concat = Concatenate()([conv_1, conv_2, conv_3, conv_4])
