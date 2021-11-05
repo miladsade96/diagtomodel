4eb9ef40b2a7dfb01481da3efc88f2a7b0661899
EverLookNeverSee <EverLookNeverSee@ProtonMail.ch>
2021-11-05 08:20:08 +0330

    Completing setup.py configuration

--- setup.py
+++ setup.py
@@ -6,14 +6,26 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="diagtomodel",
-    version="0.3.1",
+    version="0.3.2",
     author="Milad Sadeghi DM",
     author_email="EverLookNeverSee@ProtonMail.ch",
     description="Implementations of Artificial Neural Networks Based on their Diagrams",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/EverLookNeverSee/diag2model",
+    keywords=[
+        "Python",
+        "Machine Learning",
+        "Computer Vision",
+        "Deep Learning",
+        "Neural Networks",
+    ],
+    install_requires=[
+        "tensorflow>=2.5.0",
+        "tensorflow-gpu>=2.5.0"
+    ],
     project_urls={
+        "GitHub Pages": "https://everlookneversee.github.io/diag2model/",
         "Bug Tracker": "https://github.com/EverLookNeverSee/diag2model/issues"
     },
     classifiers=[
